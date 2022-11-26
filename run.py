111fb809c37ad6e5287a447b62674d4a4de5ae3d
Shizuka Donaghue <sdonaghue@gmail.com>
2022-11-26 07:28:50 +0000

    Initial commit with main file structure and main functions

--- run.py
+++ run.py
@@ -1,3 +1,29 @@
-# Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+from words import word_list
+from snowman import draw_snoman
+
+def game_introduction():
+
+
+
+
+def set_number_of_lives():
+
+
+
+def get_random_word():
+
+
+
+def play_game():
+
+
+
+def main():
+
+
+
+
