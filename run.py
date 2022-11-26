2942a6004b5430e5c71289ed3cd2b61bb10a23ee
Shizuka Donaghue <sdonaghue@gmail.com>
2022-11-26 09:02:53 +0000

    Add number_of_lives function

--- run.py
+++ run.py
@@ -7,18 +7,37 @@ from words import winter_words
 
 
 def game_introduction():
+    """
+    Game introduction and rules.
+    """
     print("Welcome to the Melting Snowman game!\n")
     print("This is a word guessing game.")
     print("Suggest a letter at a time to guess the word.")
-    print("You can also suggest a word if you think you've figured it out!\n")
+    print("You can also suggest a word if you think you've figured it out \\('-')/\n")
     print("You can set the difficulty by selecting the number of lives.")
-    print("Snowman will start melting for each failed attempt.")
-    print("Let's save the snowman before he melts!!\n")
+    print("The snowman will start melting for each failed attempt.")
+    print("Let's guess the word and save the snowman before he melts!!\n")
 
     main()
 
 
-# def set_number_of_lives():
+def set_number_of_lives():
+    """
+    Lets the player to select the number of lives.
+    """
+    print("Please select the number of lives.")
+
+    player_choice = False
+    while not player_choice:
+        choice = input("Enter 7 for 7 lives or 10 for 10 lives: ").upper()
+        if choice == "7":
+            number_of_lives = 7
+            return number_of_lives
+        elif choice == "10":
+            number_of_lives = 10
+            return number_of_lives
+        else:
+            print("Please select the number of lives.")
 
 
 def get_random_word():
@@ -34,6 +53,7 @@ def get_random_word():
 
 
 def main():
+    set_number_of_lives()
     get_random_word()
 
 
---
+++ snowman.py
@@ -0,0 +1,5 @@
+def draw_melting_snowman():
+    """
+    Draws a melting snowman based on the number of faiiled attempts.
+    """
+    
