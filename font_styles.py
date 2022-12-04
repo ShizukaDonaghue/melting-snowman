be59232a3e55ec18ad952af0d7ebb92ca8e0b8c2
Shizuka Donaghue <sdonaghue@gmail.com>
2022-12-04 10:04:53 +0000

    Remove unused font styles from font_styles.py

--- font_styles.py
+++ font_styles.py
@@ -2,29 +2,12 @@ class styles:
     """
     Font styles for the game.
     """
-    # RED = '\033[31m'
     GREEN = '\033[32m'
     GREENBOLD = '\033[1;32m'
     YELLOW = '\033[33m'
     YELLOWBOLD = '\033[1;33m'
-    # BLUE = '\033[34m'
-    CYAN = '\033[36m'
     CYANBOLD = '\033[1;36m'
-    CYANBLACK = '\033[1;36;40m'
-    # WHITE = '\033[37m'
-    # BLUEBACK = '\033[44m'
-    # REDBACK = '\033[41m'
-    BLACKBACK = '\033[40m'
     BOLD = '\033[1m'
-    END = '\033[0m'
-
-    # CYAN = '\033[96m'
-    # DARKCYAN = '\033[36m'
-    # BLUE = '\033[94m'
-    # GREEN = '\033[92m'
-    # YELLOW = '\033[93m'
-    # RED = '\033[91m'
-    # BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
     RESETUNDERLINE = "\033[24m"
-    # END = '\033[0m'
+    END = '\033[0m'
